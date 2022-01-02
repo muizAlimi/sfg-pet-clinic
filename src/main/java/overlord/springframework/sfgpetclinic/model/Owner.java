018afc2437e1dd9453029021a1eccbb5b09662ab
Muiz Alimi <muiz.alimi@qucoon.com>
2022-01-02 08:46:23 +0100

    adding POJOs. Closes #2

---
+++ src/main/java/overlord/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package overlord.springframework.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/overlord/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package overlord.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
