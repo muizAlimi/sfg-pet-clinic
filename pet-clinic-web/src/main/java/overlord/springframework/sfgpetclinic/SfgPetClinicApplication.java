6e5783902d9481af521d249caa5c7b8b0191fa03
Muiz Alimi <muiz.alimi@qucoon.com>
2022-01-02 10:11:55 +0100

    refactoring to multimodule maven project. closes #1

--- pet-clinic-web/src/main/java/overlord/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/overlord/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,6 +1,6 @@
 package overlord.springframework.sfgpetclinic;
 
-import  org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
