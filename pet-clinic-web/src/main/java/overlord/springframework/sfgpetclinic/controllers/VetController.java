a924d759490a13551743b518237755bd82dc8b1a
Muiz Alimi <muiz.alimi@qucoon.com>
2022-01-11 15:29:36 +0100

    Added Owners List and added controllers. closes #16

--- pet-clinic-web/src/main/java/overlord/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/overlord/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package overlord.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import overlord.springframework.sfgpetclinic.services.VetService;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @GetMapping({"/vets", "vets/index", "vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @GetMapping({"/", "/index", "/index.html"})
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
